8b09a1b10c59427a4fdbe6d42079771b9bf9e214
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-20 21:43:50 -0300

    Add migrate command on app startup.

--- Startup.cs
+++ Startup.cs
@@ -42,8 +42,13 @@ namespace PadariaTech
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DbContext context)
         {
+            if(context.Database.CanConnect())
+            {
+                context.Database.Migrate();
+            }
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
