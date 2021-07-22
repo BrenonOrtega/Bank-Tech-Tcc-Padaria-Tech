130d34ab686effec66cb782a08b57232a847245f
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-22 07:45:23 -0300

    Starts adding profile for automapper

---
+++ Profiles/BakeryProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using PadariaTech.Dtos.Create;
+using PadariaTech.Dtos.Read;
+using PadariaTech.Models;
+
+namespace PadariaTech.Profiles
+{
+    public class BakeryProfile : Profile
+    {
+        public BakeryProfile()
+        {
+            CreateMap<Product, ProductCreateDto>();
+            CreateMap<ProductReadDto, Product>();
+        }
+    }
+}
