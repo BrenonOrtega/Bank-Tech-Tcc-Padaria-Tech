acab3e6a305b16ea640f305d983e8d38fa6857f9
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-26 20:39:54 -0300

    criando métodos post e delete

--- Controllers/IngredientController.cs
+++ Controllers/IngredientController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using PadariaTech.Services;
 using PadariaTech.Dtos.Create;
+using System.Threading.Tasks;
 
 namespace PadariaTech.Controllers
 {
@@ -32,10 +33,27 @@ namespace PadariaTech.Controllers
         [HttpPost]
         [ProducesResponseType((int)HttpStatusCode.Created)]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        public IActionResult Post([FromBody] IngredientCreateDto ingredientDto)
+        public async Task<IActionResult> Post([FromBody] IngredientCreateDto ingredientDto)
         {
+            var id = _ingredientService.Register(ingredientDto);
+            await _ingredientService.CommitChangesAsync();
 
+            return CreatedAtAction(nameof(Post), new { id }, new { id, ingredientDto.Name, ingredientDto.Measurement, ingredientDto.Quantity });
         }
 
+        [HttpDelete]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredient = _ingredientService.GetById(id);
+
+            if(ingredient is null) return NotFound();
+
+            _ingredientService.Delete(id);
+            await _ingredientService.CommitChangesAsync();
+
+            return Ok();
+        }
     }
 }
