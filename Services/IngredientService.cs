7e63191523eec2e3ed0a649d76bb69dafcb346e4
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-26 20:00:11 -0300

    métodos getall e getbyid

--- Services/IngredientService.cs
+++ Services/IngredientService.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using AutoMapper;
+using System.Linq;
+using PadariaTech.Dtos.Read;
 using PadariaTech.Models;
 
 namespace PadariaTech.Services
@@ -12,6 +15,31 @@ namespace PadariaTech.Services
         {
             _ingredientRepository = ingredientRepository;
             _mapper = mapper;
+        }
+
+        public IEnumerable<IngredientReadDto> GetAll()
+        {
+            var ingredients = _ingredientRepository
+                .Get(x => true)
+                .Select(x => _mapper.Map<IngredientReadDto>(x))
+                .ToList();
+            return ingredients;
+        }
+
+        public IngredientReadDto GetById(int id)
+        {
+            var ingredient = QueryById(id);
+
+            if(ingredient is null) return new IngredientReadDto();
+
+            return _mapper.Map<IngredientReadDto>(ingredient);
+        }
+
+        private Ingredient QueryById(int id)
+        {
+            var ingredient = _ingredientRepository.Get(ingr => ingr.Id.Equals(id)).FirstOrDefault();
+
+            return ingredient;
         }
     }
 }
