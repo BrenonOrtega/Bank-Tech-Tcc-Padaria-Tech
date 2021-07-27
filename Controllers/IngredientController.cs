bca4640e294ca8a69bee28700b272a31603fcf5e
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-27 15:50:07 -0300

    atualizando ingredientController

--- Controllers/IngredientController.cs
+++ Controllers/IngredientController.cs
@@ -1,42 +1,84 @@
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using PadariaTech.Models;
+using PadariaTech.Services;
+using PadariaTech.Dtos.Create;
+using System.Threading.Tasks;
 
-namespace TccPadariaTech.Controllers
+namespace PadariaTech.Controllers
 {
     [ApiController]
-    [Route("[Controller]")]
-    public class IngredientController : ControllerBase
+    [Route("api/[Controller]")]
+    class IngredientController : ControllerBase
     {
-        private readonly IIngredientRepository _repository;
-        public IngredientController(IIngredientRepository repository)
+        private readonly IngredientService _ingredientService;
+
+        public IngredientController(IngredientService ingredientService)
         {
-            _repository = repository;
+            _ingredientService = ingredientService;
         }
 
         [HttpGet]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NoContent)]
-        public async Task<IActionResult> Get()
+        public IActionResult GetAll()
         {
-            var bakedProducts = _repository.Get(x => true).ToList();
+            var ingredients = _ingredientService.GetAll();
 
-            if (bakedProducts.Any())
-                return Ok(bakedProducts);
+            if (ingredients.Any()) return Ok(ingredients);
 
             return NoContent();
+        }
+        [HttpGet("/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public IActionResult Get(int id)
+        {
+            var ingredient = _ingredientService.GetById(id);
+
+            if(ingredient is not null) return Ok(ingredient);
+
+            return NotFound();
         }
 
         [HttpPost]
         [ProducesResponseType((int)HttpStatusCode.Created)]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> Post([FromBody] Ingredient ingredient)
+        public async Task<IActionResult> Post([FromBody] IngredientCreateDto ingredientDto)
         {
-            _repository.Add(ingredient);
-            await _repository.SaveChanges();
-            return CreatedAtAction(nameof(Post), new { ingredient.Id }, ingredient);
+            var id = _ingredientService.Register(ingredientDto);
+            await _ingredientService.CommitChangesAsync();
+
+            return CreatedAtAction(nameof(Post), new { id }, new { id, ingredientDto.Name, ingredientDto.Measurement, ingredientDto.Quantity });
+        }
+
+        [HttpDelete("/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ingredient = _ingredientService.GetById(id);
+
+            if(ingredient is null) return NotFound();
+
+            _ingredientService.Delete(id);
+            await _ingredientService.CommitChangesAsync();
+
+            return NoContent();
+        }
+        [HttpPut("/{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Update(int id, [FromBody] IngredientCreateDto ingredientDto)
+        {
+            var ingredient = _ingredientService.GetById(id);
+
+            if(ingredient is null) return NotFound();
+
+            _ingredientService.Update(id, ingredientDto);
+            await _ingredientService.CommitChangesAsync();
+
+            return Ok();
         }
     }
 }
