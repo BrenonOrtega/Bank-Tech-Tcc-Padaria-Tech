175d60d626c9c8b7ed5401f791c38c5b8b26a1da
Erick Rennan Coelho <ercosc@gmail.com>
2021-07-27 18:38:42 -0300

    criando map que recebe o Dto de ingrediente e monta o Ingredient da model

--- Profiles/IngredientProfile.cs
+++ Profiles/IngredientProfile.cs
@@ -12,6 +12,8 @@ namespace PadariaTech.Profiles
             CreateMap<Product, IngredientProductReadDto>();
             CreateMap<Recipe, IngredientRecipeReadDto>();
 
+            CreateMap<IngredientCreateDto, Ingredient>();
+
             CreateMap<Ingredient, IngredientReadDto>()
                 .ForMember(readDto => readDto.Recipe, opt => opt.MapFrom(model => model.Recipe))
                 .ForMember(readDto => readDto.Product, opt => opt.MapFrom(model => model.Product));
