6e4dd40a048da849060085d0396036a15a611025
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-28 12:00:20 -0300

    Adds new Migrations

--- Data/Migrations/20210728135728_FixBakedProduct_RecipeCardinality.Designer.cs
+++ Data/Migrations/20210728135728_FixBakedProduct_RecipeCardinality.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PadariaTech.Data;
 
 namespace PadariaTech.Data.Migrations
 {
     [DbContext(typeof(BakeryContext))]
-    partial class BakeryContextModelSnapshot : ModelSnapshot
+    [Migration("20210728135728_FixBakedProduct_RecipeCardinality")]
+    partial class FixBakedProduct_RecipeCardinality
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
